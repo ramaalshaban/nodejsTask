c36ced93beac99facea53a88612d586c7d1a845e
Rama ALSHABAN <50273459+ramaalshaban@users.noreply.github.com>
2022-10-04 16:38:30 +0300

    edited

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,22 @@
-const userRole = (role) => {
-    return (req, res, next) => {
-      if (isAdmin) {
+const Employees = require("../models/employee");
+const mongoose = require('mongoose');
+const { decode } = require("jsonwebtoken");
+
+function userRole(req, res, next) {
+  const token = req.body.token;
+  if(token){
+      const decode = jwt.verify(token, 'secret');
+
+      const admin = await Employees.findOne({
+        _id: mongoose.Types.ObjectId(decode.id),
+    });
+      if (admin.isAdmin) {
         next();
       } else 
         return res.status(403)
         .json({ message: "You're not authorized" });
     };
-  };
-  
+  }
+    
   module.exports = userRole;
   
